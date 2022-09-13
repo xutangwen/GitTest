1d248d856708ca06302706f14e39bdeb280e59ff
xutangwen <352502885@qq.com>
2022-09-13 17:46:06 +0800

    dev分支添加一行

--- TestModel/src/main/java/com/test/Test.java
+++ TestModel/src/main/java/com/test/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello world.");
         System.out.println("Master添加一行");
+        System.out.println("Dev添加一行");
     }
 }
