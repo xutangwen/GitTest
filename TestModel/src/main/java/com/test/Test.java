06ce5c023e4fe0b1b6ea922a7c9b2ad359a445ef
xutangwen <352502885@qq.com>
2022-09-13 17:52:19 +0800

    master分支添加新方法

--- TestModel/src/main/java/com/test/Test.java
+++ TestModel/src/main/java/com/test/Test.java
@@ -4,4 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello world.");
     }
+    public void masterChangeTitle(){
+        System.out.println("MasterChange");
+    }
 }
