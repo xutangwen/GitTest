80286af629d24d312eabea69e4b12481e297eb38
xutangwen <352502885@qq.com>
2022-09-13 17:50:27 +0800

    添加新方法

--- TestModel/src/main/java/com/test/Test.java
+++ TestModel/src/main/java/com/test/Test.java
@@ -4,4 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello world.");
     }
+    public void changeTitle(){
+        System.out.println("更改主题");
+    }
 }
