b60cc9ae37f8eae5d8c50a2f50f5454abf796338
xutangwen <352502885@qq.com>
2022-09-13 17:49:33 +0800

    还原

--- TestModel/src/main/java/com/test/Test.java
+++ TestModel/src/main/java/com/test/Test.java
@@ -3,7 +3,5 @@ package com.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello world.");
-        System.out.println("Master添加一行");
-        System.out.println("Dev添加一行");
     }
 }
